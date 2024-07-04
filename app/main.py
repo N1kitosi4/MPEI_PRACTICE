fcaf3e9a8abd2d06cd893e0d873a291e03d38c48
N1kitosi4 <129109100+N1kitosi4@users.noreply.github.com>
2024-07-05 00:36:38 +0300

    Update docker and main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+from datetime import time
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from psycopg2.extras import RealDictCursor
+
 from app.routers import posts, users, auth, votes
 from app.pages.router import router as pages_router
+import psycopg2
 
 app = FastAPI(title="MPEI_PRACTICE")
 
@@ -22,3 +27,18 @@ app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(votes.router)
 app.include_router(pages_router)
+
+
+@app.get("/")
+def info():
+    try:
+        conn = psycopg2.connect(host='localhost', database='heh', user='postgres',
+                                password='1234', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was succesfull!")
+        cursor.execute("SELECT * FROM users")
+        users = cursor.fetchall()
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error: ", error)
+    return {"users": users}
